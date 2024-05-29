5c93418fd41f61a8a74a3c1432f553df415b6bc4
enaanai <aenaanai@uae.ac.ma>
2024-05-29 21:02:04 +0100

    test

--- SeleniumGoogleScrap.py
+++ SeleniumGoogleScrap.py
@@ -18,12 +18,19 @@ searchBox=driver.find_element(By.NAME,"q")
 searchBox.send_keys("Maroc")
 searchBox.send_keys(Keys.ENTER)
 WebDriverWait(driver,15).until(EC.presence_of_element_located((By.TAG_NAME,"h3")))
-for i in range(1,10):
+
+for i in range(1,1):
     driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
     time.sleep(2)
-titres=driver.find_elements(By.TAG_NAME,"h3")
-for titre in titres:
-    print(titre.text)
 
+blocs=driver.find_elements(By.CSS_SELECTOR,'span[jscontroller="msmzHf"]')
 
-time.sleep(50)
+for bloc in blocs:
+    if bloc.text.strip()!="":
+        try:
+            titre=bloc.find_element(By.TAG_NAME,"h3").text
+            lien=bloc.find_element(By.TAG_NAME,"a").get_attribute("href")
+            print(titre)
+            print(lien)
+            print("----------------------------------------------------------------------")
+        except: continue
